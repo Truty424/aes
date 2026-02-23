ca91783d1a599c513217d12105fb5c9622ec05d4
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-02-23 15:06:21 +0100

    Update aes.py

--- aes.py
+++ aes.py
@@ -4,23 +4,9 @@ from cryptography.hazmat.primitives.padding import PKCS7
 
 key = os.urandom(32)
 iv = os.urandom(16)
+# Create a cipher object
+cipher = Cipher(algorithms.Camellia(key), modes.OFB(iv)) # Noncompliant {{(BlockCipher) Camellia}}
 
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-
-plaintext = b"a secret message"
-
-# ---- Encrypt ----
-padder = PKCS7(algorithms.AES.block_size).padder()
-padded_data = padder.update(plaintext) + padder.finalize()
-
+# Encrypt
 encryptor = cipher.encryptor()
-ct = encryptor.update(padded_data) + encryptor.finalize()
-
-# ---- Decrypt ----
-decryptor = cipher.decryptor()
-padded_res = decryptor.update(ct) + decryptor.finalize()
-
-unpadder = PKCS7(algorithms.AES.block_size).unpadder()
-unpadded_res = unpadder.update(padded_res) + unpadder.finalize()
-
-print(unpadded_res)
+ct = encryptor.update(b"a secret message") + encryptor.finalize()
