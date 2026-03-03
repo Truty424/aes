aa288dfa068dcef8271c156bf80129ecf80b2782
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-03-03 11:31:48 +0100

    Update aes.py

--- aes.py
+++ aes.py
@@ -6,20 +6,13 @@ key = os.urandom(32)
 iv = os.urandom(16)
 nonce = os.urandom(16)
 
-plaintext = b"a secret message"
-
 cipher_enc = Cipher(algorithms.AES(key), modes.CBC(iv))
 padder = PKCS7(algorithms.AES.block_size).padder()
-padded = padder.update(plaintext) + padder.finalize()
-
-encryptor = cipher_enc.encryptor()
-ct = encryptor.update(padded) + encryptor.finalize()
+padded = padder.update(b"a secret message") + padder.finalize()
+ct = cipher_enc.encryptor().update(padded) + cipher_enc.encryptor().finalize()
 
 cipher_dec = Cipher(algorithms.AES(key), modes.CTR(nonce))
-decryptor = cipher_dec.decryptor()
-padded_res = decryptor.update(ct) + decryptor.finalize()
+padded_res = cipher_dec.decryptor().update(ct) + cipher_dec.decryptor().finalize()
 
 unpadder = PKCS7(algorithms.AES.block_size).unpadder()
-unpadded = unpadder.update(padded_res) + unpadder.finalize()
-
-print(unpadded)
+print(unpadder.update(padded_res) + unpadder.finalize())
