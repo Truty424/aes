07cfdb27c0537b554e86e1a571f7ee340bea62c6
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-03-03 11:55:32 +0100

    Update aes.py

--- aes.py
+++ aes.py
@@ -2,17 +2,15 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.padding import PKCS7
 
-key_enc = os.urandom(32)
-key_dec = os.urandom(16)
+key = os.urandom(32)
 iv = os.urandom(16)
 
-cipher_enc = Cipher(algorithms.AES(key_enc), modes.CBC(iv))
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
 padder = PKCS7(algorithms.AES.block_size).padder()
 padded = padder.update(b"a secret message") + padder.finalize()
-ct = cipher_enc.encryptor().update(padded) + cipher_enc.encryptor().finalize()
 
-cipher_dec = Cipher(algorithms.AES(key_dec), modes.CBC(iv))
-padded_res = cipher_dec.decryptor().update(ct) + cipher_dec.decryptor().finalize()
+ct = cipher.encryptor().update(padded) + cipher.encryptor().finalize()
+padded_res = cipher.decryptor().update(ct) + cipher.decryptor().finalize()
 
 unpadder = PKCS7(algorithms.AES.block_size).unpadder()
 print(unpadder.update(padded_res) + unpadder.finalize())
