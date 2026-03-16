ef37dd88fee1fcfead5d639e185381933ad20db8
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-03-16 12:40:04 +0100

    Update aes.py

--- aes.py
+++ aes.py
@@ -2,15 +2,20 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.padding import PKCS7
 
-key = os.urandom(32)
+key_aes = os.urandom(32)
 iv = os.urandom(16)
 
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+key_chacha = os.urandom(32)
+nonce_chacha = os.urandom(16)
+
+# Encrypt with AES-CBC + PKCS7
+cipher_enc = Cipher(algorithms.AES(key_aes), modes.CBC(iv))
 padder = PKCS7(algorithms.AES.block_size).padder()
 padded = padder.update(b"a secret message") + padder.finalize()
+ct = cipher_enc.encryptor().update(padded) + cipher_enc.encryptor().finalize()
 
-ct = cipher.encryptor().update(padded) + cipher.encryptor().finalize()
-padded_res = cipher.decryptor().update(ct) + cipher.decryptor().finalize()
+# Decrypt with ChaCha20
+cipher_dec = Cipher(algorithms.ChaCha20(key_chacha, nonce_chacha), mode=None)
+res = cipher_dec.decryptor().update(ct) + cipher_dec.decryptor().finalize()
 
-unpadder = PKCS7(algorithms.AES.block_size).unpadder()
-print(unpadder.update(padded_res) + unpadder.finalize())
+print(res)
